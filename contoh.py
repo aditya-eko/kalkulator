bfd3b4029e16defd9016924247227957e48277bf
aditya-eko <ekoagusaditya@gmail.com>
2019-09-14 13:35:39 +0700

    menambahkan file contoh.py

--- contoh.py
+++ contoh.py
@@ -1,37 +1,17 @@
-class kalkulator:
-    pass
+angka=int(input("masukan angka"))
+angka2=int(input("masukkan angka yang ke2 "))
+
+def tambah():
+    return angka+angka2
+
+def kurang():
+    return angka-angka2
+
+def kali():
+    return angka*angka2
+
+def bagi():
+    return angka/angka2
 
 
-    def __init__(self):
-        self.satu=1
-        self.dua=2
-        self.tiga=3
-        self.empat=4
-        self.lima=5
-        self.enam=6
-        self.tujuh=7
-        self.delapan=8
-        self.sembilan=9
-
-
-    def tambah(self,up1,up2):
-        return up1+up2
-
-    def kurang(self,up1,up2):
-        return up1-up2
-
-    def kali(self,up1,up2):
-        return up1*up2
-
-
-    def bagi(self,up1,up2):
-        return up1/up2
-
-
-
-angka=kalkulator()
-
-print(angka.tambah(8,9))
-print(angka.kurang(8,2))
-print(angka.kali(8,4))
-print(angka.bagi(9,3))
+print(tambah())
